349f5176538376f1c45f7459b59adc26c53b95f6
yapokoffiregis <regisbergan@gmail.com>
2022-06-02 21:21:26 -0700

    task 4 commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,12 @@
 /**
  * using puts function
  *
- *
  * Return: Always 0 (succees)
- *
  */
 
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
