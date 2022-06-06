fb59a0294386bd6622377f601693648b7593a844
yapokoffiregis <regisbergan@gmail.com>
2022-06-06 13:20:01 -0700

    task 3 commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	int i, j;
+	char i, j;
 	for (i = 97; i <= 122; i++)
 	{
-	putchar(i);
+		putchar(i);
 	}
 	for (j = 65; j <= 90; j++)
 	{
-	putchar(j);
+		putchar(j);
 	}
 	putchar('\n');
 
