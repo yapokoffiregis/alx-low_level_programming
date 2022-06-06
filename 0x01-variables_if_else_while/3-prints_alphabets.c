46f30e684f69888241919906b5c62e9a7a6d21c0
yapokoffiregis <regisbergan@gmail.com>
2022-06-06 12:54:05 -0700

    task 3 commit

---
+++ 0x01-variables_if_else_while/3-prints_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*
+*main - alphabet lowercase then uppercase
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+    int i, j;
+for(i = 97; i <= 122; i++)
+    {
+        putchar(i);
+    }
+    for(j = 65; j <= 90; j++)
+    {
+        putchar(j);
+    }
+    putchar('\n');
+
+    return 0;
+}
