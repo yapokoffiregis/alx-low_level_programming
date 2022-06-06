89686004109ac631bc240306d1207d4178556984
yapokoffiregis <regisbergan@gmail.com>
2022-06-06 14:53:21 -0700

    task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - base 10 digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return 0;
+}
