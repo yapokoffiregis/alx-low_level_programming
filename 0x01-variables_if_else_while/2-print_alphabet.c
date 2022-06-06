f0788d9a632c6827f456e9b3100b4fa551122c10
yapokoffiregis <regisbergan@gmail.com>
2022-06-06 11:06:58 -0700

    task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - alphabet in lowercase
+ *  
+ *  Return: Always 0 (succes)
+ */
+
+int main()
+{
+	int alphabet;
+
+	for (alphabet = 97; alphabet <= 122; alphabet++)
+	{
+		putchar(alphabet);
+		putchar('\n');
+	}
+return (0);
+
+}
