c5fb522ee324a663ffc4ac9b17a59ffd5e839090
yapokoffiregis <regisbergan@gmail.com>
2022-06-06 15:08:59 -0700

    task 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+	char tab[26];
+	tab="abcdfghijklmnoprstuvwxyz";
+
+	for (i = 0; i <= 24; i++)
+		putchar(tab[i]);
+	putchar('\n');
+	return (0);
+}
