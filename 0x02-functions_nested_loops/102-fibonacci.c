92a875821ebfdb04709ab149cabd5fcf63229bd5
yapokoffiregis <regisbergan@gmail.com>
2022-06-07 22:08:03 -0700

    task14 commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,9 +3,8 @@
 /**
  * main - print fibonacci
  *
- * Return: 0
+ * Return: always 0
  */
-
 int main(void)
 {
 	long int a, b, c, d;
@@ -14,11 +13,11 @@ int main(void)
 	b = 2;
 	c = 3;
 
-	printf("%ld, ",a);
+	printf("%ld, ", a);
 	printf("%ld, ", b);
 	for (d = 3; d < 50; d++)
 	{
-		printf("%ld ", c);
+		printf("%ld, ", c);
 		a = b;
 		b = c;
 		c = a + b;
