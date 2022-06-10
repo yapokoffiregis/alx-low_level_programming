2ffb098e373762d44264d95ecad68a0a7907f835
yapokoffiregis <regisbergan@gmail.com>
2022-06-09 22:26:16 -0700

    task 1 commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks digits
+ *@c: digit to check
+ *
+ * Return: 1 if is digit 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
