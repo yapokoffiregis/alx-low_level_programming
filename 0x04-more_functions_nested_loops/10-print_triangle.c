7e714d286212e0111462b7d4a876f2b97e12cad9
yapokoffiregis <regisbergan@gmail.com>
2022-06-09 22:53:31 -0700

    task 10 commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
- * print_triangle - Print a triangle
- * @size: size of the triangle.
- *
- * Return: no return.
- */
+* print_triangle - prints a triangle line with hashtags
+* @size: an int given by main
+* Same as my 7 print diagonal. check that file and its comments
+* Description: Uses headers to link and a nested loops to achieve goal
+* can do one nested for instead of two. do a if j to i equality check
+* Return: void. no return.
+*/
 
 void print_triangle(int size)
 {
 	int i, j;
 
+	if (size <= 0)
+		_putchar('\n');
 	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j < (size - i); j++)
-		{
+		for (j = size - i - 1; j > 0; j--)
 			_putchar(' ');
-		}
-		for (j--; j < size; j++)
-		{
-			_putchar(35);
-		}
-		if (i < (size - 1))
-			_putchar('\n');
+		for (j = i + 1; j > 0; j--)
+			_putchar('#');
+		_putchar('\n');
 	}
-	_putchar('\n');
-
 }
