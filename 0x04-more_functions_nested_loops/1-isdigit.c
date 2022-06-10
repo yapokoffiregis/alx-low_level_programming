00db955ee5c6fc4676846abe08b5c1f8af4601bb
yapokoffiregis <regisbergan@gmail.com>
2022-06-09 22:31:35 -0700

    task 1 commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * _isupper - checks is a character is uppercase
- * @c: carrier integer variable
- * Return: 1 if true, 0 if false
+ * _isdigit - Checks if parameter is a number between 0 to 9.
+ *  @c: input number.
+ *
+ *  Return: 1 if c is a digit and 0 if otherwise.
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 48 && c < 58)
 	{
 		return (1);
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
