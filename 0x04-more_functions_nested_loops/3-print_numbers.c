c5e81b4167bbadb84afe023c8cdf6208e3a332ee
yapokoffiregis <regisbergan@gmail.com>
2022-06-10 11:27:48 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * @: print 0 to 9.
+ *
+ * Return: 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+}
