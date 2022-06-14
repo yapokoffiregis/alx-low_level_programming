24cfea7d71c621e5c564d61e479d89c10c511e8f
yapokoffiregis <regisbergan@gmail.com>
2022-06-13 20:24:56 -0700

    task 0 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98.c - Entry point
- *
- * @n: pointer
+ * reset_to_98.c - Takes a pointer to an integer parameter and 
+ * updates the value it points to to 98.
+ * @n: input integer.
  *
  * Return: 0 .
  */
@@ -12,5 +12,4 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 	
-	return (0);
 }
