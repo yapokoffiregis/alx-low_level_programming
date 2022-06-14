c49eee47d1d0652ac3552a0dd9d2c91b6699042e
yapokoffiregis <regisbergan@gmail.com>
2022-06-13 20:55:59 -0700

    task 11 comomit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,23 +3,40 @@
 #include <time.h>
 
 /**
- * main - generates keygen.
- * Return: Always 0.
+ * main - random password generator for 101-crackme
+ *
+ * Return: always 0
  */
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
+	srand(time(NULL));
+	while (s != 2772)
 	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
+		i = k = s = 0;
+		while ((2772 - 122) > s)
+		{
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
+		}
 	}
-	printf("%c\n", (2772 - c));
+	p[i] = '\0';
+	printf("%s", p);
 	return (0);
 }
