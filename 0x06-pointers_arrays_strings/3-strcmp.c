189c558e08d0f3799c895e958e99bb8076d995c7
yapokoffiregis <regisbergan@gmail.com>
2022-06-16 19:38:49 -0700

    task 3 commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,28 +1,19 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings.
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes used from src.
- * Return: the pointer to dest.
+ * _strcmp - Compares pointers to two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
+ *
+ * Return: 0 is success.
  */
-char *_strncat(char *dest, char *src, int n)
+int _strcmp(char *s1, char *s2)
 {
-	int count = 0, count2 = 0;
-
-	while (*(dest + count) != '\0')
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		count++;
+		s1++;
+		s2++;
 	}
 
-	while (count2 < n)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
-	return (dest);
+	return (*s1 - *s2);
 }
