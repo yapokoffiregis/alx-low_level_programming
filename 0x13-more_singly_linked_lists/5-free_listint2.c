69da5a9a6651b3f58fbda0a1cd4617a31a8bb0da
yapokoffiregis <regisbergan@gmail.com>
2022-07-19 22:33:50 -0700

    task 5 and 6 commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list.
+ * @head: pointer to the beginning node.
+ *
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
+		return;
+
+	while ((*head)->next != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	free(*head);
+	*head = NULL;
+}
