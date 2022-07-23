39f1dc34d9f01f6b039a4de8da790e3c9c90f319
yapokoffiregis <regisbergan@gmail.com>
2022-07-22 21:57:45 -0700

    task 0 commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,29 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: pointing to a string of 0 and 1 chars.
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: points to a string of 0 and 1 chars
  *
- * Return: 0 if there is one or  more chars in the string b
- * that  is not 0 or 1 or b is NULL otherwise return the
- * converted number.
+ * Return: converted number, otherwise 0 of b is NULL or if there
+ * is one or more chars in string b thats not 0 or 1
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, len, j;
-	unsigned int total = 0;
-	unsigned int decval = 1;
+	unsigned int value = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	if (len == 1 && (b[0] == '0' || b[0] == '1'))
-		return ((b[0] - 48));
-	for (i = 0; b[i] != '\0'; i++)
+	while (*b != '\0')
 	{
-		if (b[i] != '1' && b[i] != '0')
+		value = value << 1;
+		if (*b != '1' && *b != '0')
 			return (0);
-		for (j = len - 1; j > 0; j--)
-			decval = decval * 2;
-		total = total + (decval * (b[i] - 48));
-		len--;
-		decval = 1;
+		else if (*b == '1')
+			value = value | 1;
+		b++;
 	}
-
-	return (total);
+	return (value);
 }
